66ba7b6f66e76ec4886d9098372df823ec23060f
Mauricio Vater <mauriciovater21@gmail.com>
2021-07-15 00:53:54 -0300

    First commit MV

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+        ];
+    }
+
+    public function with($request){
+        return [
+            'api_author' => 'Mauricio Vater',
+        ];
+    }
+}
